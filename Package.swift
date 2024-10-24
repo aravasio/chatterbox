4e66193f3448587badd3628675145ed83c6335ca
Ale Ravasio <alejandroravasio@gmail.com>
2024-10-24 11:31:51 -0300

    Set minimum macOS deployment target to 10.15 in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "chatterbox",
+    platforms: [
+        .macOS(.v10_15) // Specify macOS 10.15 since I depend on the use of Task {}
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0")
